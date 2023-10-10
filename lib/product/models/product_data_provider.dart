db4daf3027f8e8705b5d3826aa99563e00eb0a91
Dmitry <mitya.lyashko.02@mail.ru>
2023-10-10 18:32:56 +0300

    add provider

---
+++ lib/product/models/product_data_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:start_project/product/models/api/products_api.dart';
+import 'package:start_project/product/models/entities/product_data_api_model.dart';
+
+class ProductDataProvider extends ValueNotifier<List<ProductModel>> {
+  ProductDataProvider({required this.categoryId}) : super([]);
+  int categoryId;
+
+  ProductsApi api = ProductsApi();
+
+  bool isLoading = false;
+  bool allDataLoaded = false;
+  int limitSet = 15;
+
+
+  Future<void> loadNextItems() async {
+    //реализовать логику загрузки данных через api с контролем overflow
+    if (isLoading || allDataLoaded) return;
+
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      final productList = await api.loadList(
+        categoryId: categoryId,
+        offSet: value.length,
+      );
+      if (productList.isEmpty) {
+        allDataLoaded = true;
+        print('overflow-можно сделать уведомление');
+      } else {
+        value.addAll(productList);
+      }
+    } catch (error) {
+      print('Что-то пошло не так');
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> reload() async {
+    value.clear();
+    allDataLoaded = false;
+    notifyListeners();
+    await loadNextItems();
+  }
+}
+
--- lib/product/product_list_page.dart
+++ lib/product/product_list_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:start_project/product/models/api/products_api.dart';
 import 'package:start_project/product/models/entities/product_data_api_model.dart';
+import 'package:start_project/product/models/product_data_provider.dart';
 import 'package:start_project/product/product_details_page.dart';
 import 'package:start_project/views/network_image_widget.dart';
 import 'package:start_project/product/view/product_list_view/product_price_widget.dart';
@@ -19,44 +19,20 @@ class ProductListPage extends StatefulWidget {
 
 class _ProductListPageState extends State<ProductListPage> {
   final scrollController = ScrollController();
-  int setNumber = 0;
-  List<ProductModel> products = [];
-  bool isLoading = false;
-  bool hasMoreData = true;
-
-  void _getData() async {
-    if (isLoading || !hasMoreData) return;
-
-    setState(() {
-      isLoading = true;
-    });
-
-    final productList = await ProductsApi()
-        .loadList(categoryId: widget.categoryId, offSet: setNumber);
-
-    setState(() {
-      products.addAll(productList);
-      isLoading = false;
-      if (productList.length < 15) {
-        hasMoreData = false;
-      }
-    });
-  }
+  late ProductDataProvider productDataProvider;
 
   @override
   void initState() {
     super.initState();
+    productDataProvider = ProductDataProvider(categoryId: widget.categoryId);
     scrollController.addListener(_scrollListener);
-    _getData();
+    productDataProvider.loadNextItems();
   }
 
   void _scrollListener() {
-    const int limitSet = 15;
-    if ((scrollController.position.pixels ==
-            scrollController.position.maxScrollExtent) &&
-        (hasMoreData == true)) {
-      setNumber = setNumber + limitSet;
-      _getData();
+    if (scrollController.position.pixels ==
+        scrollController.position.maxScrollExtent) {
+      productDataProvider.loadNextItems();
     }
   }
 
@@ -103,6 +79,9 @@ class _ProductListPageState extends State<ProductListPage> {
       ),
     );
   }
+  Future<void> _refresh() async {
+    productDataProvider.reload();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -113,62 +92,68 @@ class _ProductListPageState extends State<ProductListPage> {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: ListView.builder(
-          controller: scrollController,
-          itemCount: (isLoading || !hasMoreData)
-              ? products.length + 1
-              : products.length,
-          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-          itemBuilder: (context, index) {
-            if (index < products.length) {
-              final product = products[index];
-              return Container(
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(5)),
-                  color: Theme.of(context).colorScheme.inversePrimary,
-                ),
-                margin: const EdgeInsets.only(bottom: 5),
-                child: Row(
-                  children: [
-                    InkWell(
-                      child: NetworkImageWidget(
-                        img: product.imageUrl,
-                        height: 150,
-                        width: 150,
-                        margin: 5,
+        child: RefreshIndicator(
+          onRefresh: _refresh,
+          child: ValueListenableBuilder<List<ProductModel>>(
+            valueListenable: productDataProvider,
+            builder: (context, productList, child) {
+              return ListView.builder(
+                controller: scrollController,
+                itemCount: productList.length + ((productDataProvider.isLoading||productDataProvider.allDataLoaded) ? 1 : 0),
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                itemBuilder: (context, index) {
+                  if (index < productList.length) {
+                    final product = productList[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(5)),
+                        color: Theme.of(context).colorScheme.inversePrimary,
                       ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                ProductDetailPage(productId: product.productId),
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            child: NetworkImageWidget(
+                              img: product.imageUrl,
+                              height: 150,
+                              width: 150,
+                              margin: 5,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ProductDetailPage(
+                                      productId: product.productId),
+                                ),
+                              );
+                            },
                           ),
-                        );
-                      },
-                    ),
-                    _drawProductInfo(product.title, product.productDescription,
-                        product.price)
-                  ], //children
-                ),
+                          _drawProductInfo(product.title,
+                              product.productDescription, product.price)
+                        ], //children
+                      ),
+                    );
+                  } else if (productDataProvider.isLoading) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (productDataProvider.allDataLoaded) {
+                    return const Center(
+                      child: Text(
+                        'это все товары',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    );
+                  } else {
+                    return const EmptyCategoryPage();
+                  }
+                }, //children
               );
-            } else if (isLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (products.isNotEmpty) {
-              return const Center(
-                child: Text(
-                  'это все товары',
-                  style: TextStyle(
-                    fontSize: 10,
-                  ),
-                ),
-              );
-            } else {
-              return const EmptyCategoryPage();
-            }
-          }, //children
+            },
+          ),
         ),
       ),
     );
